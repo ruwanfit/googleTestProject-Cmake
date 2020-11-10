182fc4ffd8313b9ecabe1475dff74c7bb5c22d46
Ruwan Ananda <ruwans@simct.com>
2020-11-10 18:28:28 +0530

    added build folder step 1
    Signed-off-by: Ruwan Ananda <ruwans@simct.com>

---
+++ build/Source.cpp
@@ -0,0 +1,12 @@
+//#include<stdio.h>
+#include "UnitTest.h"
+#include <iostream>
+
+int main()
+{
+	//int i = 1;
+	std::cout << "Hello jenkins...";
+	pldl::UnitTest utest = pldl::UnitTest();
+	return 0;
+	//pldl::UnitTest unitTest;
+}
